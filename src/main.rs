c41de1b29ec99032df43ccf244561f195d2027f4
nicholasf <nicholas.faiz@gmail.com>
2024-06-25 12:52:55 +1000

    Command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // let args: Vec<String> = env::args().collect();
+    let filename = env::args().nth(1).expect("No filename given.");
+    let word = env::args().nth(2).expect("No word given.");
+    println!("filename: {}", filename);
+    println!("word: {}", word);
 }
