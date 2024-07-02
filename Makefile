345862cd6f64d9fa837acb1480453935aa270aca
nicholasf <nicholas.faiz@gmail.com>
2024-07-02 12:36:45 +1000

    Migrations working

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 $PHONY: up, down
 
 up:
-	migrate -path migrations/ -database "postgresql://$(DB_USER):$(DB_PASSWORD)@localhost:5432/word_counter?sslmode=disable" -verbose up
+	migrate -path migrations/ -database "postgresql://$(DB_USER):$(DB_PASSWORD)@$(DB_HOST):5432/word_counter?sslmode=disable" -verbose up
--- migrations/000001_base.up.sql
+++ migrations/000001_base.up.sql
@@ -1,15 +1,15 @@
-create table files {
-    id  int,
+create table files (
+    id  int not null primary key,
     name varchar(120)
-}
+);
 
-create table lines {
-    id int,
+create table lines (
+    id int not null primary key,
     file_id int references files(id)
-}
+);
 
-create table words {
-    id int,
+create table words (
+    id int not null primary key,
     file_id int references lines(id),
     word varchar(120)
-}
+);
